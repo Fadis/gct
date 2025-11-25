00bd7586d88b1fe11a6fbce5a1f5d46e99ed4b35
Fadis <fadis@quaternion.sakura.ne.jp>
2025-11-26 02:28:42 +0900

    remove unneeded code

--- include/gct/shader/glsl/gct/xpbd/rigid_constraint.h
+++ include/gct/shader/glsl/gct/xpbd/rigid_constraint.h
@@ -16,7 +16,6 @@ struct rigid_collision_dx_dq {
   uint dq_count;
 };
 
-// per constraint
 rigid_collision_dx_dq rigid_collision_constraint_dx(
   uint constraint_offset,
   uint constraint_index,
@@ -28,16 +27,6 @@ rigid_collision_dx_dq rigid_collision_constraint_dx(
   );
   bool is_end = rigid_collision_constraint_is_end( iter );
 
-  /*if( is_end ) {
-    return rigid_collision_dx_dq(
-      vec3( 0, 0, 0 ),
-      vec4( 0, 0, 0, 0 ),
-      0,
-      0
-    );
-  }*/
-
-
   const uvec2 dc =
     is_end ? uvec2( 0, 0 ) :
     uvec2(
@@ -48,10 +37,10 @@ rigid_collision_dx_dq rigid_collision_constraint_dx(
   const uint rigid0 = particle_pool[ dc.x ].rigid;
   const vec3 p0 = particle_pool[ dc.x ].position;
   const vec3 p_prev0 = particle_pool[ dc.x ].previous_position;
+  const vec4 q0 = matrix_pool[ rigid_pool[ rigid0 ].trs ][ 1 ];
   const vec3 dir0 = p0 - p_prev0;
   const vec3 n0 = particle_pool[ dc.x ].normal;
   const vec3 r0 = particle_pool[ dc.x ].local_r;
-  const vec4 q0 = matrix_pool[ rigid_pool[ rigid0 ].trs ][ 1 ];
   const float inv_m0 = 1.0/rigid_pool[ rigid0 ].mass;
   const mat3 inv_i0 = mat3( matrix_pool[ rigid_pool[ rigid0 ].inversed_momentum_inertia_tensor ] );
 
@@ -67,64 +56,18 @@ rigid_collision_dx_dq rigid_collision_constraint_dx(
   if( dot( n0, n1 ) >= 0.0 ) {
     is_end == true;
   }
-  /*if( dot( dir0 - dir1, n1 ) >= 0.0 ) {
-    is_end == true;
-  }
-  if( dot( dir1 - dir0, n0 ) >= 0.0 ) {
-    is_end == true;
-  }*/
-/*  
-  const float c = distance( p0, p1 );
-  const vec3 n = ( p1 - p0 )/c;
-
-  const float w0 = inv_m0 + dot( cross( r0, n ), inv_i0 * cross( r0, n ) );
-  
-  const float w1 = inv_m1 + dot( cross( r1, n ), inv_i1 * cross( r1, n ) );
-  
-  const float alpha = 100.0;
-  const float alpha_dt2 = alpha / ( dt * dt );
-  
-  const float dl = ( -c ) / ( w0 + w1 + alpha_dt2 );
-
-  const vec3 p = n * dl;
-  const vec3 dx = p * inv_m0;
-  const vec4 dq = quaternion_quaternion_mult( 1.0/2.0 * vec4( inv_i0 * cross( r0, p ), 0.0 ), q0 );
-
-  vec3 dx_sum = subgroupAdd( dx );
-  uint dx_count = subgroupAdd( dx == vec3( 0, 0, 0 ) ? 0u : 1u );
-  vec4 dq_sum = subgroupAdd( dq );
-  uint dq_count = subgroupAdd( dq == vec4( 0, 0, 0, 0 ) ? 0u : 1u );
-*/
 
   const float c = distance( p0, p1 );
 
-  if( c < 0.0001 )  {
+  if( c < 0.000001 )  {
     is_end = true;
   }
 
-  /*if( abs( c ) < 0.0001 ) {
-    return rigid_collision_dx_dq(
-      vec3( 0, 0, 0 ),
-      vec4( 0, 0, 0, 0 ),
-      0,
-      0
-    );
-  }*/
-
   const vec3 n = n1;// normalize( p0 - p1 );
 
-  /*if( dot( n, n0 ) <= 0.0 ) {
-    return rigid_collision_dx_dq(
-      vec3( 0, 0, 0 ),
-      vec4( 0, 0, 0, 0 ),
-      0,
-      0
-    );
-  }*/
-
-  const float w0 = inv_m0 + dot( cross( r0, n ), inv_i0 * cross( r0, n ) );
+  const float w0 = inv_m0 + dot( cross( r0, n1 ), inv_i0 * cross( r0, n1 ) );
   
-  const float w1 = inv_m1 + dot( cross( r1, n ), inv_i1 * cross( r1, n ) );
+  const float w1 = inv_m1 + dot( cross( r1, n0 ), inv_i1 * cross( r1, n0 ) );
   
   const float alpha = 0.000005;
   const float alpha_dt2 = alpha / ( dt * dt );
@@ -144,21 +87,14 @@ rigid_collision_dx_dq rigid_collision_constraint_dx(
     1.0/2.0 * quaternion_quaternion_mult( vec4( inv_i0 * cross( r0, sqrt( 1.0 - r * r ) * p ), 0.0 ), q0 ) +
     1.0/2.0 * quaternion_quaternion_mult( vec4( inv_i0 * cross( r0, delta_pt * dlf ), 0.0 ), q0 );
 
-  //dx *= 0.01;
-  //dq *= 0.01;
   dx = is_end ? vec3( 0, 0, 0 ) : dx;
   dq = is_end ? vec4( 0, 0, 0, 0 ) : dq;
 
-  vec3 dx_sum = subgroupAdd( dx );
-  uint dx_count = subgroupAdd( dx == vec3( 0, 0, 0 ) ? 0u : 2u );
-  vec4 dq_sum = subgroupAdd( dq );
-  uint dq_count = subgroupAdd( dq == vec4( 0, 0, 0, 0 ) ? 0u : 2u );
-
   return rigid_collision_dx_dq(
-    dx_sum,
-    dq_sum,
-    dx_count,
-    dq_count
+    dx,
+    dq,
+    dx == vec3( 0, 0, 0 ) ? 0u : 2u,
+    dq == vec4( 0, 0, 0, 0 ) ? 0u : 2u
   );
 }
 
@@ -240,115 +176,74 @@ rigid_collision_dx_dq rigid_border_solve(
     dq_count
   );
 }
-/*
-rigid_collision_dx_dq rigid_border_friction(
-  vec3 p0,
-  vec3 p_prev0,
-  vec4 q0,
-  vec3 n0,
-  vec3 r0,
-  float inv_m0,
-  mat3 inv_i0,
-  vec3 p1,
-  float dt
-) {
-  const float c = distance( p0, p1 );
-  const vec3 n = ( p0 - p1 )/c;
 
-
-  const vec3 dx = -delta_pt * dlf;
-
-  vec3 dx_sum = subgroupAdd( dx );
-  uint dx_count = subgroupAdd( dx == vec3( 0, 0, 0 ) ? 0u : 1u );
-  vec4 dq_sum = vec4( 0, 0, 0, 0 );
-  uint dq_count = 0;
-
-  return rigid_collision_dx_dq(
-    dx_sum,
-    dq_sum,
-    dx_count,
-    dq_count
-  );
-}
-*/
-
-// per particle
 rigid_collision_dx_dq rigid_border_dx(
   uint rigid_id,
-  uint particle_offset,
-  uint unique_vertex_count,
+  uint global_particle_id,
   aabb_type aabb,
   float dt
 ) {
-  const vec3 center_of_mass0 = matrix_pool[ rigid_pool[ rigid_id ].trs ][ 0 ].xyz;
-  const vec4 q0 = matrix_pool[ rigid_pool[ rigid_id ].trs ][ 1 ];
-  const mat3 inv_i0 = mat3( matrix_pool[ rigid_pool[ rigid_id ].inversed_momentum_inertia_tensor ] );
-  const float inv_m0 = 1.0/rigid_pool[ rigid_id ].mass;
-  const float alpha = 100.0;
-  
   vec3 dx = vec3( 0.0, 0.0, 0.0 );
   vec4 dq = vec4( 0.0, 0.0, 0.0, 0.0 );
   uint dx_count = 0u;
   uint dq_count = 0u;
-  for( uint vertex_id = gl_SubgroupInvocationID; vertex_id < unique_vertex_count; vertex_id += gl_SubgroupSize ) {
-    const vec3 p0 = particle_pool[ particle_offset + vertex_id ].position;
-    const vec3 p_prev0 = particle_pool[ particle_offset + vertex_id ].previous_position;
-    const vec3 n0 = particle_pool[ particle_offset + vertex_id ].normal;
-    const vec3 r0 = particle_pool[ particle_offset + vertex_id ].local_r;
-    if( p0.x > aabb.max.x ) {
-      const vec3 p1 = vec3( aabb.max.x, p0.y, p0.z );
-      rigid_collision_dx_dq d = rigid_border_solve( p0, p_prev0, q0, n0, r0, inv_m0, inv_i0, p1, dt );
-      dx += d.dx;
-      dq += d.dq, dq;
-      dx_count += d.dx_count;
-      dq_count += d.dq_count;
-    }
-    else if( p0.x < aabb.min.x ) {
-      const vec3 p1 = vec3( aabb.min.x, p0.y, p0.z );
-      rigid_collision_dx_dq d = rigid_border_solve( p0, p_prev0, q0, n0, r0, inv_m0, inv_i0, p1, dt );
-      dx += d.dx;
-      dq += d.dq, dq;
-      dx_count += d.dx_count;
-      dq_count += d.dq_count;
-    }
-    if( p0.y > aabb.max.y ) {
-      const vec3 p1 = vec3( p0.x, aabb.max.y, p0.z );
-      rigid_collision_dx_dq d = rigid_border_solve( p0, p_prev0, q0, n0, r0, inv_m0, inv_i0, p1, dt );
-      dx += d.dx;
-      dq += d.dq;
-      dx_count += d.dx_count;
-      dq_count += d.dq_count;
-    }
-    else if( p0.y < aabb.min.y ) {
-      const vec3 p1 = vec3( p0.x, aabb.min.y, p0.z );
-      rigid_collision_dx_dq d = rigid_border_solve( p0, p_prev0, q0, n0, r0, inv_m0, inv_i0, p1, dt );
-      dx += d.dx;
-      dq += d.dq, dq;
-      dx_count += d.dx_count;
-      dq_count += d.dq_count;
-    }
-    if( p0.z > aabb.max.z ) {
-      const vec3 p1 = vec3( p0.x, p0.y, aabb.max.z );
-      rigid_collision_dx_dq d = rigid_border_solve( p0, p_prev0, q0, n0, r0, inv_m0, inv_i0, p1, dt );
-      dx += d.dx;
-      dq += d.dq;
-      dx_count += d.dx_count;
-      dq_count += d.dq_count;
-    }
-    else if( p0.z < aabb.min.z ) {
-      const vec3 p1 = vec3( p0.x, p0.y, aabb.min.z );
-      rigid_collision_dx_dq d = rigid_border_solve( p0, p_prev0, q0, n0, r0, inv_m0, inv_i0, p1, dt );
-      dx += d.dx;
-      dq += d.dq, dq;
-      dx_count += d.dx_count;
-      dq_count += d.dq_count;
-    }
+  const mat3 inv_i0 = mat3( matrix_pool[ rigid_pool[ rigid_id ].inversed_momentum_inertia_tensor ] );
+  const float inv_m0 = 1.0/rigid_pool[ rigid_id ].mass;
+  
+  const vec3 p0 = particle_pool[ global_particle_id ].position;
+  const vec3 p_prev0 = particle_pool[ global_particle_id ].previous_position;
+  const vec4 q0 = matrix_pool[ rigid_pool[ rigid_id ].trs ][ 1 ];
+  const vec3 n0 = particle_pool[ global_particle_id ].normal;
+  const vec3 r0 = particle_pool[ global_particle_id ].local_r;
+  if( p0.x > aabb.max.x ) {
+    const vec3 p1 = vec3( aabb.max.x, p0.y, p0.z );
+    rigid_collision_dx_dq d = rigid_border_solve( p0, p_prev0, q0, n0, r0, inv_m0, inv_i0, p1, dt );
+    dx += d.dx;
+    dq += d.dq, dq;
+    dx_count += d.dx_count;
+    dq_count += d.dq_count;
+  }
+  else if( p0.x < aabb.min.x ) {
+    const vec3 p1 = vec3( aabb.min.x, p0.y, p0.z );
+    rigid_collision_dx_dq d = rigid_border_solve( p0, p_prev0, q0, n0, r0, inv_m0, inv_i0, p1, dt );
+    dx += d.dx;
+    dq += d.dq, dq;
+    dx_count += d.dx_count;
+    dq_count += d.dq_count;
+  }
+  if( p0.y > aabb.max.y ) {
+    const vec3 p1 = vec3( p0.x, aabb.max.y, p0.z );
+    rigid_collision_dx_dq d = rigid_border_solve( p0, p_prev0, q0, n0, r0, inv_m0, inv_i0, p1, dt );
+    dx += d.dx;
+    dq += d.dq;
+    dx_count += d.dx_count;
+    dq_count += d.dq_count;
+  }
+  else if( p0.y < aabb.min.y ) {
+    const vec3 p1 = vec3( p0.x, aabb.min.y, p0.z );
+    rigid_collision_dx_dq d = rigid_border_solve( p0, p_prev0, q0, n0, r0, inv_m0, inv_i0, p1, dt );
+    dx += d.dx;
+    dq += d.dq, dq;
+    dx_count += d.dx_count;
+    dq_count += d.dq_count;
+  }
+  if( p0.z > aabb.max.z ) {
+    const vec3 p1 = vec3( p0.x, p0.y, aabb.max.z );
+    rigid_collision_dx_dq d = rigid_border_solve( p0, p_prev0, q0, n0, r0, inv_m0, inv_i0, p1, dt );
+    dx += d.dx;
+    dq += d.dq;
+    dx_count += d.dx_count;
+    dq_count += d.dq_count;
+  }
+  else if( p0.z < aabb.min.z ) {
+    const vec3 p1 = vec3( p0.x, p0.y, aabb.min.z );
+    rigid_collision_dx_dq d = rigid_border_solve( p0, p_prev0, q0, n0, r0, inv_m0, inv_i0, p1, dt );
+    dx += d.dx;
+    dq += d.dq, dq;
+    dx_count += d.dx_count;
+    dq_count += d.dq_count;
   }
 
-  dx = subgroupAdd( dx );
-  dq = subgroupAdd( dq );
-  dx_count = subgroupAdd( dx_count );
-  dq_count = subgroupAdd( dq_count );
   return rigid_collision_dx_dq(
     dx,
     dq,
